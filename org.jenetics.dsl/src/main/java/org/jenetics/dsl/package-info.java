4f8faef6d751ce8de115ff2ffea967fa455627fb
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2014-01-14 17:39:23 +0100

    Add new 'org.jenetics.dsl' project.

---
+++ org.jenetics.dsl/src/main/java/org/jenetics/dsl/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
+ * @since @__version__@
+ * @version @__version__@ &mdash; <em>$Date: 2014-01-14 $</em>
+ */
+package org.jenetics.dsl;
