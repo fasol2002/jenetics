c78ac4298e21d687cf41f1f75df57ab93af47759
Franz Wilhelmstötter <franz.wilhelmstoetter@gmail.com>
2015-01-07 16:12:29 +0100

    Make project compile again after merge of 'default' branch.

--- org.jenetics.dsl/src/main/java/org/jenetics/dsl/Foo.java
+++ org.jenetics.dsl/src/main/java/org/jenetics/dsl/Foo.java
@@ -1,6 +1,9 @@
+package org.jenetics.dsl;
+
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
  * @since !__version__!
  * @version !__version__! &mdash; <em>$Date: 2015-01-07 $</em>
  */
-package org.jenetics.dsl;
+public class Foo {
+}
--- org.jenetics.dsl/src/test/java/org/jenetics/dsl/package-info.java
+++ org.jenetics.dsl/src/test/java/org/jenetics/dsl/package-info.java
@@ -1,6 +1,6 @@
 /**
  * @author <a href="mailto:franz.wilhelmstoetter@gmx.at">Franz Wilhelmstötter</a>
- * @since @__version__@
- * @version @__version__@ &mdash; <em>$Date: 2014-01-14 $</em>
+ * @since !__version__!
+ * @version !__version__! &mdash; <em>$Date: 2015-01-07 $</em>
  */
 package org.jenetics.dsl;
